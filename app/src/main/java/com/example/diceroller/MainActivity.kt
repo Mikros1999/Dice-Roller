1e9dc34e49a619a83857cf45c557e3bdea4b3ce0
Mikros1999 <galaxyldrago0196@gmail.com>
2022-04-20 15:26:57 +0200

    added appropriate variables and function for random number generation and setting image to appropriate one

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rollBtn: Button
+    private lateinit var diceImage: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        diceImage = findViewById(R.id.imageView)
+
+        rollBtn=findViewById(R.id.buttonOne)
+
+        rollBtn.setOnClickListener(){
+            rollDice()
+        }
+
     }
+
+    private fun rollDice() {
+        var randomInt:Int = Random.nextInt(6)+1
+        var drawableResource = when(randomInt) {
+            1 -> R.drawable.die1
+            2 -> R.drawable.die2
+            3 -> R.drawable.die3
+            4 -> R.drawable.die4
+            5 -> R.drawable.die5
+            else -> R.drawable.die6
+        }
+
+        diceImage.setImageResource(drawableResource)
+    }
+
 }
